7db3c1aed1fbfc39090997d40f4b1c074077fc1e
TomekoLeko <ays@vp.pl>
2025-03-20 17:16:56 +0100

    feat: StockReduction type, peding status of an order

--- products/models.py
+++ products/models.py
@@ -103,6 +103,7 @@ class CartItem(models.Model):
 
 class Order(models.Model):
     STATUS_CHOICES = [
+        ('pending', 'Oczekujące'),
         ('accepted', 'Przyjęte'),
         ('shipped', 'Nadane'),
         ('delivered', 'Dostarczone'),
@@ -114,7 +115,7 @@ class Order(models.Model):
     status = models.CharField(
         max_length=20, 
         choices=STATUS_CHOICES, 
-        default='accepted'
+        default='pending'
     )
     created_at = models.DateTimeField(auto_now_add=True)
     total_cost = models.DecimalField(max_digits=10, decimal_places=2)
